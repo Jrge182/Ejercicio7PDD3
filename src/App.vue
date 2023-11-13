101038a0273080b50ef62baae796c7c14bdc82df
Jorge <91501563+Jrge182@users.noreply.github.com>
2023-11-12 19:02:58 -0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<!-- App.vue -->
+
+<template>
+  <div id="app">
+    <ShoppingList :items="shoppingItems" />
+  </div>
+</template>
+
+<script>
+import ShoppingList from "./ShoppingList.vue";
+
+export default {
+  components: {
+    ShoppingList
+  },
+  data() {
+    return {
+      shoppingItems: ["Manzanas", "Leche", "Sabritas", "Coca-Cola", "Jugo"]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+
+/* Estilos opcionales para la aplicación principal */
+h2 {
+  color: #333;
+}
+</style>
---
+++ src/ShoppingList.vue
@@ -0,0 +1,33 @@
+<!-- ShoppingList.vue -->
+
+<template>
+  <div>
+    <h2>Lista de Compras</h2>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Estilos opcionales para el componente */
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 5px;
+}
+</style>
